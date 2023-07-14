ee564fa7c03c61c45aa3fcf1678fe7b31fc91398
MuhammadFauzandotexe <mfauzan51087@gmail.com>
2023-07-14 19:16:28 +0700

    [Initilize]

---
+++ src/test/java/org/ojan/PaymentService/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ojan.PaymentService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
